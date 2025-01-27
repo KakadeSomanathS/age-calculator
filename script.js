e5036e2407366b8c18118ac777a51e8b35b71bb1
Somanath Shahaji kakade <somanath.kakade5@gmail.com>
2025-01-27 23:14:37 +0530

    added submit event for event listiner

--- script.js
+++ script.js
@@ -3,15 +3,28 @@ const calculateBtn = document.querySelector("#calculateBtn");
 const year = document.querySelector("#year");
 const month = document.querySelector("#month");
 const day = document.querySelector("#day");
+const form = document.querySelector("#form");
 
-const birthDate = new Date(inputDate.value);
-const currentDate = new Date();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-calculateBtn.addEventListener("click", () => {
-  const birthYear = birthDate.getFullYear() - currentDate.getFullYear();
-  year.textContent = birthYear;
-  const birthMonth = birthDate.getMonth() - currentDate.getMonth();
-  month.textContent = birthMonth;
-  const birthDay = birthDate.getDay() - currentDate.getDay();
-  day.textContent = birthDay;
+  const birthDateValue = inputDate.value;
+
+  if (birthDateValue === "") {
+    alert("Please enter a valid past date!");
+  } else {
+    console.log("Success");
+
+    const birthDate = new Date(birthDateValue);
+    const currentDate = new Date();
+    const dateYears = currentDate.getFullYear() - birthDate.getFullYear();
+
+    const monthEle = birthDate.getMonth() - currentDate.getMonth();
+    console.log(dateYears);
+    year.textContent = dateYears;
+    month.textContent = monthEle;
+
+    const daysEle = birthDate.getDay() - currentDate.getDay();
+    day.textContent = daysEle;
+  }
 });
